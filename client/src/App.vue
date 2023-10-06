8d9eaeacbcf70f987f0537e11f39b713beca02be
rabume <info@rabu.me>
2023-10-06 18:47:21 +0200

    WIP - refactor, rename, formatting

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <Map msg="CS2_DMA_Radar" />
+    <Map msg="CS2_DMA_Radar" />
 </template>
 
 <script setup>
-import Map from "./components/Map.vue";
+import Map from './components/Map.vue'
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,12 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "leaflet/dist/leaflet.css";
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'leaflet/dist/leaflet.css'
 
-import * as L from "leaflet";
-import "leaflet/dist/leaflet.css";
-import "leaflet.pm";
-import "leaflet.pm/dist/leaflet.pm.css";
-import "leaflet-rotatedmarker";
+import * as L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet.pm'
+import 'leaflet.pm/dist/leaflet.pm.css'
+import 'leaflet-rotatedmarker'
 
-const app = createApp(App);
-app.mount("#app");
+const app = createApp(App)
+app.mount('#app')
