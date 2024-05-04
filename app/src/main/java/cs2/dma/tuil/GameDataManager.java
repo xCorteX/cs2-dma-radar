37a3cc1859f3f4a23ac7adb949ad4f9a8198c4f5
rabume <info@rabu.me>
2024-05-04 08:25:30 +0200

    update mapname offset #35

--- app/src/main/java/cs2/dma/tuil/GameDataManager.java
+++ app/src/main/java/cs2/dma/tuil/GameDataManager.java
@@ -93,7 +93,7 @@ public class GameDataManager {
         memoryTool = new MemoryTool(gameProcess);
         clientAddress = memoryTool.getModuleAddress("client.dll");
         mapNameAddress = memoryTool.getModuleAddress("matchmaking.dll");
-        mapNameAddress = memoryTool.readAddress(mapNameAddress + 0x001D2300, 8);
+        mapNameAddress = memoryTool.readAddress(mapNameAddress + 0x001D23B0, 8); // dwGameTypes_mapName
         EntityList = memoryTool.readAddress(clientAddress + dwEntityList, 8);
         EntityList = memoryTool.readAddress(EntityList + 0x10, 8);
 
