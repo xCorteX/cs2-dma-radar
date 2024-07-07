dd633a9574544c7f6f631eac5480a9e9eba26aad
rabume <info@rabu.me>
2024-07-07 09:52:31 +0200

    add new maps to knowMap variable #39

--- app/src/main/java/cs2/dma/tuil/GameDataManager.java
+++ app/src/main/java/cs2/dma/tuil/GameDataManager.java
@@ -46,7 +46,7 @@ public class GameDataManager {
         }
     }
 
-    private String knowMap = "de_ancient,de_dust2,de_inferno,de_mirage,de_nuke,de_overpass,de_vertigo,de_anubis,cs_office,ar_baggage,ar_shoots";
+    private String knowMap = "de_ancient,de_dust2,de_inferno,de_mirage,de_nuke,de_overpass,de_vertigo,de_anubis,cs_office,ar_baggage,ar_shoots,de_thera,de_mills";
     private static String[] argvMemProcFS = { "", "-device", "FPGA" };
 
     private static IVmmProcess gameProcess;
